87d355ab3492d85b0db6b10b54be695220458b91
Rubem Azevedo Mazzetto <87815929+RubemAZ@users.noreply.github.com>
2025-01-27 00:29:24 -0300

    update question 3 and 4

--- Prova/src/app/pages/question4/question4.component.ts
+++ Prova/src/app/pages/question4/question4.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './question4.component.scss'
 })
 export class Question4Component {
+  dados = [
+    {
+      estado: 'MG',
+      valorMensal: 0
+    },
+    {
+      estado: 'MG',
+      valorMensal: 0
+    },
+    {
+      estado: 'SP',
+      valorMensal: 0
+    },
+    {
+      estado: 'RJ',
+      valorMensal: 0
+    },
+    {
+      estado: 'ES',
+      valorMensal: 0
+    },{
+      estado: 'ES',
+      valorMensal: 0
+    },
+  ]
 
+  teste() {
+    const item
+  }
 }
