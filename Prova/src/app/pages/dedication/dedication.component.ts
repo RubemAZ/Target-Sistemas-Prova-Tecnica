5946594299e56848c35334c82a5589002e2145e7
Rubem Azevedo Mazzetto <87815929+RubemAZ@users.noreply.github.com>
2025-01-27 07:10:53 -0300

    ADD Dedication page

---
+++ Prova/src/app/pages/dedication/dedication.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dedication',
+  imports: [],
+  templateUrl: './dedication.component.html',
+  styleUrl: './dedication.component.scss'
+})
+export class DedicationComponent {
+
+}
